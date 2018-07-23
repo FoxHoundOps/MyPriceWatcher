0790153c85067860a56c136f5759998addad545f
Damian Najera <dgnajera2@miners.utep.edu>
2018-07-22 19:10:15 -0600

    Initial commit for DeleteDialog and its handler, DeleteDialogListener.

---
+++ app/src/main/java/edu/utep/cs/cs4330/mypricewatcher/DeleteDialog.java
@@ -0,0 +1,56 @@
+package edu.utep.cs.cs4330.mypricewatcher;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatDialogFragment;
+
+/**
+ *  A custom AlertDialog to display when a user tries to delete an item from their list of items
+ *  that will be tracked. This dialog will allow a user to confirm whether or not they actually
+ *  want to proceed with deleting the item from their list.
+ *
+ * @author Damian Najera
+ * @version 1.0
+ */
+public class DeleteDialog extends AppCompatDialogFragment {
+    private DeleteDialogListener listener;      /* The internal listener for this DeleteDialog */
+
+    /**
+     * Build and return a DialogFragment that prompts the user whether or not they want to continue
+     * to delete an item from their list. Initializes handling of the positive/negative buttons. If
+     * the positive button is selected, communicate back through the listener with a boolean 'true'. If
+     * the negative button is selected, communicate back through the listener with an boolean 'false'.
+     * .
+     * @param savedInstanceState The saved instance state from the activity that invoked the dialog
+     * @return  A DeleteDialog.
+     */
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.delete_dialog_prompt)
+                .setPositiveButton(R.string.yes_dialog_button_label, (dialogInterface, i) -> listener.onResponse(this, true))
+                .setNegativeButton(R.string.no_dialog_button_label, (dialogInterface, i) -> listener.onResponse(this, false));
+        setCancelable(false);
+        return builder.create();
+    }
+
+    /**
+     * Attach the implemented DeleteDialogListener from the activity interested in the response from
+     * the DeleteDialog dialog.
+     *
+     * @param context The Context whose implemented DeleteDialogListener will be attached to the DeleteDialog dialog.
+     */
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            listener = (DeleteDialogListener) context;
+        } catch(ClassCastException e) {
+            throw new ClassCastException(context.toString() + "Need to implement DeleteDialogListener.");
+        }
+    }
+}
---
+++ app/src/main/java/edu/utep/cs/cs4330/mypricewatcher/DeleteDialogListener.java
@@ -0,0 +1,19 @@
+package edu.utep.cs.cs4330.mypricewatcher;
+
+/**
+ *  Listener to be implemented when using the DeleteDialog dialog. This listener allows
+ *  for the DeleteDialog to send a response describing what button was selected by a user
+ *  on the dialog.
+ *
+ * @author Damian Najera
+ * @version 1.0
+ */
+public interface DeleteDialogListener {
+    /**
+     * The method to be implemented by whichever activity invokes the DeleteDialog dialog, so that
+     * the activity can receive a response describing what a user selected from the dialog.
+     *
+     * @param proceed A boolean describing whether a user selected the positive or negative button
+     */
+    void onResponse(DeleteDialog d, boolean proceed);
+}
